dd711f26dcfd6ef0aeba98e8681cd5b460f36b76
Taras Shemeiko <shemiks2005@gmail.com>
2025-09-19 20:36:54 +0300

    task completed

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-$('button.continue').html('Next Step...')
+let $li10 = $('li:eq(9)').css('color', 'red')
+let $parent = $li10.parent()
+let $parentSibling = $parent.prev()
+$parentSibling.wrap('<div class="wwww"></div>')
