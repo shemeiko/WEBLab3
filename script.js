8d2954aed382c7e2eaf7ab0f49e8686b8acb1426
Taras Shemeiko <shemiks2005@gmail.com>
2025-09-19 21:03:49 +0300

    replace #test element with element with .www

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let $li10 = $('li:eq(9)').css('color', 'red')
 let $parent = $li10.parent()
 let $parentSibling = $parent.prev()
 $parentSibling.wrap('<div class="wwww"></div>')
+
+// 2
+let $test = $('#test').css('color', 'green')
+let $elWithWWW = $test.next('.wwww')
+$test.insertAfter($elWithWWW)
