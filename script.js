a8ccad0c404083f90e80b8447e4e986d5d872bcd
Taras Shemeiko <shemiks2005@gmail.com>
2025-09-23 20:00:25 +0300

    init

---
+++ script.js
@@ -0,0 +1,68 @@
+let $li10 = $('li:eq(9)').css('color', 'red')
+let $parent = $li10.parent()
+let $parentSibling = $parent.siblings().not('div').not('span')
+$parentSibling.wrapAll('<div class="www">')
+
+// 1
+let $test = $('#test').css('color', 'green')
+let $elWithWWW = $test.next('.www')
+$test.insertAfter($elWithWWW)
+
+// 2
+$('p').each(function() {
+    let $p = $(this)
+
+    if (!$p.parent().is('div.www')) {
+        $p.unwrap()
+        $p.wrap('<div class=".zzz">')
+    }
+})
+
+// 3
+// console.log($('#test'))
+
+// 4
+$('p').each(function() {
+    let $className = $(this).attr('class')
+    if ($className) {
+        $(this).wrap(`<div class=${$className}>`)
+        $(this).removeAttr('class')
+    }
+});
+
+// 5
+$('li').slice(-4, -1)
+    .each(function() {
+        $(this).html('!') 
+    })
+
+// 6
+
+// 7
+
+// 8
+$('li').each(function() {
+    if($(this).hasClass('.www')) {
+        $(this).css('color', 'red')
+        if ($(this).hasClass('.bbb')) {
+            $(this).remove()
+        }
+    }
+})
+
+// 9
+let $li = $('li')
+$li.even().html('')
+$li.odd().remove()
+
+// 10
+$('div').each(function() {
+    $(this).css('height', `${$(this).height() * 2}px`)
+})
+
+// 11
+let sum = 0;
+$('div').each(function() {
+    sum += $(this).height()
+})
+console.log(sum)
